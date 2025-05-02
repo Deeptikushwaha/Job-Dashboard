60743069a5d43475501efb370260f8c6cfea19e7
Deeptikushwaha <deeptikushwaha140@gmail.com>
2025-05-02 17:41:37 +0530

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "../components/Footer"
-import Frontsection from "../components/Frontsection";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
